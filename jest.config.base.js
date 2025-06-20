dc66ae1bf93163bc22eb85f8fcbf9f2b61a787f1
Scott Sandler <ssandler@makenotion.com>
2025-06-20 13:41:54 -0600

    disable coverage threshold in jest config

--- jest.config.base.js
+++ jest.config.base.js
@@ -10,13 +10,4 @@ module.exports = {
   ],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   collectCoverage: true,
-  // TODO: these treshold values should be 100 for all, but at this time they didn't have 100 coverage
-  coverageThreshold: {
-    global: {
-      branches: 85,
-      functions: 85,
-      lines: 93,
-      statements: 93,
-    },
-  },
 };
